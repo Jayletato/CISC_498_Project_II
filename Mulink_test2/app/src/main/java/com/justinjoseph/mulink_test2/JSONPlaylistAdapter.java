29371de3ac5e67cd6dec9e71c17997bd0fd7e326
Justin Joseph <jayjtrainer@gmail.com>
2025-03-25 09:02:18 -0400

    Forgot to commit popup layout

---
+++ Mulink_test2/app/src/main/java/com/justinjoseph/mulink_test2/JSONPlaylistAdapter.java
@@ -0,0 +1,14 @@
+package com.justinjoseph.mulink_test2;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+
+
+public class JSONPlaylistAdapter {
+    public void jsonFileReader(){
+        //return jsonobject
+        return;
+    }
+}
